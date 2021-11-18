271f09f219a459ad1ad8986ca7ceb4398d4f83ed
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-18 09:18:03 -0300

    Variaveis

---
+++ src/main/java/VariaveisDadoOperadoresAritmeticos.java
@@ -0,0 +1,51 @@
+public class VariaveisDadoOperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        int _1a; //não é boa prática
+        int $aq; // não é boa prática
+
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+
+        final int j = 10; //final= constante
+        //j = 15; constante não pode ser mudada
+        int asrn24678md;
+        int asrn24678_md =10; //não é boa prática
+
+        asrn24678md = 100;
+        asrn24678_md = 10;
+
+
+        int quantidadeProduto = 50; //boa prática CamelCase
+        int QuantidadeProduto; //não boa prática
+        final int NUMERO_TENTATIVAS = 5; //constante deve seguir essa regra
+        final int numeroTentativas = 5; // não boa prática
+        int QUANTIDADE_OPCOES = 25; //não boa prática
+        int qtdProd; //não fica expressiva
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn24678_md);
+
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+
+
+
+    }
+}
