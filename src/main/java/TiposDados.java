c8382a63daab788c450c58c75ead808e954d35ae
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-18 09:58:47 -0300

    Tipos de Dados

---
+++ src/main/java/TiposDados.java
@@ -0,0 +1,60 @@
+public class TiposDados {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        short s2 = 30000;
+
+        int i1 = -190000000;
+        int i2 = 28500;
+
+        long l1= 100000000L;
+        long l2 = 200400500050005500L;
+
+        float f1 = 4.5f;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.845d;
+
+        char c1 = 'A';
+        char c2 = '\u0057';
+
+        String st1= "Fulano";
+        String st2 = "Beltrano";
+        String st3 = " Podemos escrever frases com números: 12345 e usar caracteres ($%), operadores [+-*] além de aspas 'internas'";
+
+        String dt1 ="18/11/2021";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(s2 );
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(dt1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+
+    }
+}
