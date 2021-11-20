00b27058849b521631a7c461fb8d09666a4dfb95
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-20 17:14:39 -0300

    Arrays

---
+++ src/main/java/Arrays/OrdemInversa.java
@@ -0,0 +1,31 @@
+package Arrays;
+/*
+Crie um vetor de 6 números inteiros e mostre-os na ordem inversa
+ */
+
+
+public class OrdemInversa {
+
+    public static void main(String[] args) {
+
+        int [] vetor = {-2, -5, 15, 8, 9, 50};
+
+        //System.out.println(vetor.length);
+
+        System.out.print("Vetor: ");
+
+        int count = 0;
+
+        while (count < (vetor.length)){
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nVetor ordem inversa: ");
+        for(int i = (vetor.length-1); i >= 0; i--){
+            System.out.print(vetor[i] + " ");
+
+        }
+
+    }
+}
